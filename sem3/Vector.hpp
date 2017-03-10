cc9b7bd8fa89b9b440b27a469d1fc39cb223d9f6
Vlad <vlad.miryaha@gmail.com>
2017-03-10 21:09:11 +0300

    fixit
    read comments

--- sem3/Vector.hpp
+++ sem3/Vector.hpp
@@ -3,9 +3,16 @@
 
 struct Vector2 {
     double x, y;
+    
+    /*
+        fixit: вынесите реализацию конструкторов в cpp файл
+    */
     Vector2(): x(0), y(0){}
     Vector2(double X, double Y): x(X), y(Y){}
-    //Vector2(const Vector2 other);
+    
+    /*
+        fixit: += должен вернуть ссылку на себя обносленного по аналогии со встроенными типами вроде int, float и т.д.
+    */
     Vector2 operator+=(const Vector2& other);
     Vector2 operator+(const Vector2& other) const;
     Vector2 operator-=(const Vector2& other);
@@ -20,8 +27,15 @@ struct Vector2 {
     Vector2 per() const;
     Vector2 operator-() const;
     Vector2 getRotated(const double alfa) const;
+    
+    /*
+     fixit: rotate должен вернуть ссылку на себя обновленного ... по аналогии с +=
+    */
     Vector2 rotate(const double alfa);
 
+    /*
+        Почему вы vector*double сделали внешней ф-ей, а оператор + реализовали внутри класса?
+    */
     friend Vector2 operator*(const Vector2& a, const double x);
     friend Vector2 operator*(const double x, const Vector2& a);
     friend std::istream& operator >>(std::istream& stream, Vector2& vec);
